9d74cae047add8b6cb25df728f93af1d5d6cf7b3
liyaSileshi <liyasileshi@gmail.com>
2019-09-02 23:50:54 -0700

    third try

--- MadLibs.py
+++ MadLibs.py
@@ -1,3 +1,5 @@
+#Liya Tilahun
+#MadLibs example is from the project spec doc
 print("Welcome to MadLibs!!!!!")
 
 def user_input(prompt):
@@ -7,50 +9,32 @@ def user_input(prompt):
     return user_input
 
 def adj():
-    return input("Enter an adj")
-t1= "It was a "
-t2="{adjective}".format(adjective = user_input("Enter an adjective: "))
-t3=", cold November day."
-t4=" I woke up to the"
-t5=" {adjective}".format(adjective= user_input("Enter an adjective: "))
-t6=" smell of"
-t7="{bird}".format(bird=user_input("Enter a type of bird: "))
-t8= "roasting in the"
-t9="{room}".format(room= user_input("Enter a room in a house: "))
-t10="downstairs. "
-t11="I"
-t12="{verb_past}".format(verb_past=user_input("Enter a verb in past tense: "))
-t13="down the stairs to see if I could help"
-t14="{verb}".format(verb= user_input("Enter a verb: "))
-t15="the dinner. "
-t16='My mom said, "See if"'
-t17="{relative}".format(relative=user_input("Enter a relative's name: "))
-t18="needs a fresh"
-t19="{noun}.".format(noun=user_input("Enter a noun: "))
-t20= " So I carried a tray of glasses full of"
-t21="{liquid}".format(liquid=user_input("Enter a liquid: ")
-t22="into the"
-t23="{verb}".format(verb=user_input("Enter a verb ending in -ing: "))
-t24="room."
-t25= " When I got there, I couldn't believe my"
-t26="{body}!".format(body=user_input("Enter a part of the body (plural): "))
-t27= " There were"
-t28="{plural_noun}".format(plural_noun= user_input("Enter a plural noun: "))
-t29="{verb}".format(verb=user_input("Enter a verb: "))
-t30="on the"
-t31="{noun}".format(noun=user_input("Enter a noun:"))
-t32="!"
+    return input("Enter an adjective: ")
 
-adjectives=[t2, t5]
-bird=[t7]
-room=[t9]
-verb_past=[t12]
-verb=[t14,t23,t29]
-relative=[t17]
-noun=[t19,t31]
-liquid=[t21]
-body=[t26]
-plural_noun=[t28]
+
+sen1= "It was a {adjective}, cold November day.".format(adjective = user_input("Enter an adjective: "))
+sen2=" I woke up to the {adjective} smell of {bird} roasting in the {room} downstairs.".format(adjective= user_input("Enter an adjective: "),
+    bird=user_input("Enter a type of bird: "), room= user_input("Enter a room in a house: "))
+sen3= "I {verb_past} down the stairs to see if I could help {verb} the dinner.".format(verb_past=user_input("Enter a verb in past tense: "),
+    verb= user_input("Enter a verb: "))
+sen4='My mom said, "See if {relative} needs a fresh {noun}"'.format(relative=user_input("Enter a relative's name: "),noun=user_input("Enter a noun: ") )
+sen5= " So I carried a tray of glasses full of {liquid} into the {verb} room.".format(liquid=user_input("Enter a liquid: "),
+    verb=user_input("Enter a verb ending in -ing: ")
+sen6= " When I got there, I couldn't believe my {body}!".format(body=user_input("Enter a part of the body (plural): "))
+sen7= " There were {plural_noun} {verb} on the {noun}!".format(plural_noun= user_input("Enter a plural noun: "),verb=user_input("Enter a verb: ")
+    noun=user_input("Enter a noun:"))
+
+
+#adjectives=[t2, t5]
+#bird=[t7]
+#room=[t9]
+#verb_past=[t12]
+#verb=[t14,t23,t29]
+#relative=[t17]
+#noun=[t19,t31]
+#liquid=[t21]
+#body=[t26]
+#plural_noun=[t28]
 
 
 
