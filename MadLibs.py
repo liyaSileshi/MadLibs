b0fa31c6f14a41db1aa0594c06b68b99b1fc917e
liyaSileshi <liyasileshi@gmail.com>
2019-09-03 02:16:41 -0700

    fifth update

--- MadLibs.py
+++ MadLibs.py
@@ -2,27 +2,40 @@
 #MadLibs example is from the project spec doc
 print("Welcome to MadLibs!!!!!")
 
+# the input function will display a message in the terminal
+# and wait for user input.
 def user_input(prompt):
-    # the input function will display a message in the terminal
-    # and wait for user input.
     user_input = input(prompt)
     return user_input
 
 def adj():
-    return input("Enter an adjective: ")
+    return user_input("Enter an adjective: ")
 
+def bird():
+    return user_input("Enter a type of bird: ")
 
-sen1= "It was a {adjective}, cold November day.".format(adjective = user_input("Enter an adjective: "))
-sen2="I woke up to the {adjective} smell of {bird} roasting in the {room} downstairs.".format(adjective= user_input("Enter an adjective: "),
-    bird=user_input("Enter a type of bird: "), room= user_input("Enter a room in a house: "))
-sen3= "I {verb_past} down the stairs to see if I could help {verb} the dinner.".format(verb_past=user_input("Enter a verb in past tense: "),
-    verb= user_input("Enter a verb: "))
-sen4='My mom said, "See if {relative} needs a fresh {noun}"'.format(relative=user_input("Enter a relative's name: "),noun=user_input("Enter a noun: ") )
+def room():
+    return user_input("Enter a room in a house: ")
+
+def verb_past():
+    return user_input("Enter a verb in past tense: ")
+
+def verb():
+    return user_input("Enter a verb: ")
+
+def noun():
+    return user_input("Enter a noun: ")
+sen1= "It was a {adjective}, cold November day.".format(adjective =adj())
+sen2="I woke up to the {adjective} smell of {bird} roasting in the {room} downstairs.".format(adjective= adj(),
+    bird=bird(), room=room())
+sen3= "I {verb_past} down the stairs to see if I could help {verb} the dinner.".format(verb_past= verb_past(),
+    verb= verb())
+sen4='My mom said, "See if {relative} needs a fresh {noun}"'.format(relative=user_input("Enter a relative's name: "),noun= noun())
 sen5= "So I carried a tray of glasses full of {liquid} into the {verb} room.".format(liquid=user_input("Enter a liquid: "),
     verb=user_input("Enter a verb ending in -ing: "))
 sen6 = "When I got there, I couldn't believe my {body}!".format(body=user_input("Enter a part of the body: "))
-sen7= "There were {plural_noun} {verb} on the {noun}!".format(plural_noun= user_input("Enter a plural noun: "),verb=user_input("Enter a verb: "),
-    noun=user_input("Enter a noun:"))
+sen7= "There were {plural_noun} {verb} on the {noun}!".format(plural_noun= user_input("Enter a plural noun: "),verb=verb(),
+    noun=noun())
 
 print(" ")
 print("Generating story...")
