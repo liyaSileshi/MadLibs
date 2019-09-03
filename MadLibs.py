74fc4f4c76aa780a61b7c7f94c9aae0a95f8dbb8
liyaSileshi <liyasileshi@gmail.com>
2019-09-03 12:39:17 -0700

    styling updates

--- MadLibs.py
+++ MadLibs.py
@@ -25,23 +25,25 @@ def verb():
 
 def noun():
     return user_input("Enter a noun: ")
-sen1= "It was a {adjective}, cold November day.".format(adjective =adj())
-sen2="I woke up to the {adjective} smell of {bird} roasting in the {room} downstairs.".format(adjective= adj(),
-    bird=bird(), room=room())
-sen3= "I {verb_past} down the stairs to see if I could help {verb} the dinner.".format(verb_past= verb_past(),
-    verb= verb())
-sen4='My mom said, "See if {relative} needs a fresh {noun}"'.format(relative=user_input("Enter a relative's name: "),noun= noun())
-sen5= "So I carried a tray of glasses full of {liquid} into the {verb} room.".format(liquid=user_input("Enter a liquid: "),
-    verb=user_input("Enter a verb ending in -ing: "))
-sen6 = "When I got there, I couldn't believe my {body}!".format(body=user_input("Enter a part of the body: "))
-sen7= "There were {plural_noun} {verb} on the {noun}!".format(plural_noun= user_input("Enter a plural noun: "),verb=verb(),
-    noun=noun())
+
+sen1 = "It was a {adjective}, cold November day.".format(adjective = adj())
+sen2 = "I woke up to the {adjective} smell of {bird} roasting in the {room} downstairs.".format(adjective = adj(),
+    bird = bird(), room = room())
+sen3 = "I {verb_past} down the stairs to see if I could help {verb} the dinner.".format(verb_past = verb_past(),
+    verb = verb())
+sen4 = 'My mom said, "See if {relative} needs a fresh {noun}"'.format(relative = user_input("Enter a relative's name: "),noun = noun())
+sen5 = "So I carried a tray of glasses full of {liquid} into the {verb} room.".format(liquid = user_input("Enter a liquid: "),
+    verb = user_input("Enter a verb ending in -ing: "))
+sen6 = "When I got there, I couldn't believe my {body}!".format(body = user_input("Enter a part of the body: "))
+sen7 = "There were {plural_noun} {verb} on the {noun}!".format(plural_noun = user_input("Enter a plural noun: "),verb = verb(),
+    noun =noun())
 
 print(" ")
 print("Generating story...")
 print(" ")
+
 #creating a list and storing the sentences in the list
-story=[sen1, sen2, sen3, sen4, sen5, sen6, sen7]
+story = [sen1, sen2, sen3, sen4, sen5, sen6, sen7]
 
 #accessing the list using for loop
 for words in story:
